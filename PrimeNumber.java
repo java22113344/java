8d530d8d585868e695ba8d7b1ddf452939f99767
java22113344 <ganeshgrl005@gmail.com>
2025-03-27 10:28:54 +0530

    Update PrimeNumber.java

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 public class PrimeNumbers { 
      public static void main(String[] args) {  
         Scanner scanner = new Scanner(System.in); 
-        // Step 1: Get user input 
+   
            System.out.print("Enter an integer: "); 
            int n = scanner.nextInt(); 
  
-          // Step 2: Loop from 2 to n 
+
           System.out.println("Prime numbers up to " + n + ":"); 
            for (int i = 2; i <= n; i++) { 
               boolean isPrime = true; 
  
-            // Step 3: Check if i is prime 
+          
             for (int j = 2; j < i; j++) { 
                 if (i % j == 0) { 
                     isPrime = false; 
@@ -19,12 +19,12 @@ public class PrimeNumbers {
                 } 
             } 
  
-            // Step 4: If prime, print it 
+        
             if (isPrime) { 
                 System.out.print(i + " "); 
             } 
         } 
-        System.out.println();  // New line after output 
+        System.out.println(); 
         scanner.close(); 
     } 
 } 
