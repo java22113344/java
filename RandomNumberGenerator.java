7cb1de892e47de30e2d65498ebb58fea8e519d74
java22113344 <ganeshgrl005@gmail.com>
2025-03-27 10:29:42 +0530

    Update RandomNumberGenerator.java

--- RandomNumberGenerator.java
+++ RandomNumberGenerator.java
@@ -7,19 +7,19 @@ public class RandomNumberGenerator {
         Scanner scanner = new Scanner(System.in); 
  
  
-        // Step 1: Get the lower and upper limits from the user 
+
         System.out.print("Enter the lower limit: "); 
         int lower = scanner.nextInt(); 
         System.out.print("Enter the upper limit: "); 
         int upper = scanner.nextInt(); 
  
-        // Step 2: Create a Random object to generate random numbers 
+       
         Random random = new Random(); 
  
-        // Step 3: Generate a random number between the lower and upper limits (inclusive) 
+      
         int randomNumber = random.nextInt(upper - lower + 1) + lower; 
  
-        // Step 4: Print a message based on the generated value 
+ 
         System.out.println("Generated Random Number: " + randomNumber); 
          
         if (randomNumber < (lower + upper) / 3) { 
@@ -30,7 +30,6 @@ public class RandomNumberGenerator {
             System.out.println("The value is too high."); 
         } 
  
-        // Step 5: End the program 
         scanner.close(); 
     } 
 } 
