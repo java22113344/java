876e30ef849d5d6f2154785af2c77ac45d1990cc
java22113344 <ganeshgrl005@gmail.com>
2025-03-27 10:25:27 +0530

    Update ExceptionDemo.java

--- ExceptionDemo.java
+++ ExceptionDemo.java
@@ -2,32 +2,30 @@ public class ExceptionDemo {
  
     public static void main(String[] args) { 
  
-        // Demonstrating ArithmeticException 
       
    try { 
-            int result = 10 / 0;  // Division by zero 
+            int result = 10 / 0;  
         } catch (ArithmeticException e) { 
             System.out.println("ArithmeticException: Cannot divide by zero!"); 
         } 
  
-        // Demonstrating NumberFormatException 
+    
         try { 
-            int number = Integer.parseInt("abc123");  // Invalid number format 
+            int number = Integer.parseInt("abc123"); 
         } catch (NumberFormatException e) { 
             System.out.println("NumberFormatException: Invalid number format!"); 
         } 
  
-        // Demonstrating ArrayIndexOutOfBoundsException 
+        
        try { 
             int[] arr = new int[5]; 
-            arr[10] = 100;  // Index out of bounds 
+            arr[10] = 100;   
         } catch (ArrayIndexOutOfBoundsException e) { 
             System.out.println("ArrayIndexOutOfBoundsException: Index out of bounds!"); 
         } 
  
-        // Demonstrating NegativeArraySizeException 
         try { 
-            int[] arr = new int[-5];  // Negative array size 
+            int[] arr = new int[-5];  
         } catch (NegativeArraySizeException e) { 
             System.out.println("NegativeArraySizeException: Array size cannot be negative!"); 
         } 
